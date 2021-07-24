d0ad84acfc50dc5e72b987281cd938cc3df67243
pzohaycuoi <pzohaycuoi@gmail.com>
2021-07-24 17:33:13 +0700

    planning on main.ps1 file

---
+++ main.ps1
@@ -0,0 +1,18 @@
+# Check version of powershell #
+######################################################################################################################
+# Require powershell core 6.0 or above
+
+# Check existance of required powershell module #
+######################################################################################################################
+# Required powershell module:
+# 1. AD module
+# 2. Msol module
+# 3. Exchange module
+
+# If module not exist then install #
+######################################################################################################################
+# Set trusted repository - so powershell won't ask for confirmation
+# Installation
+
+# Check again if installation success or not - if not then throw #
+######################################################################################################################
